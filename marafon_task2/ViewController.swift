ec69a80e64ee6632f15f2b9dfe558ed2888f3e0f
Nokhrin Vitaly <nokhrinvitaly@gmail.com>
2023-05-06 22:26:58 +0500

    Initial Commit

---
+++ marafon_task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  marafon_task2
+//
+//  Created by Виталий Нохрин on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
